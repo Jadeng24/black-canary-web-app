5bf3fbbef8c9a68252c9b278052506019109bb01
Abigail Thelin <abbyjthelin@gmail.com>
2017-09-29 14:20:52 -0600

    setting up the profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react'
 import io from 'socket.io-client';
+import editIcon from '../../images/whiteEditIcon.svg'
+
 const socket = io('http://localhost:3069');
 
 export default class Profile extends Component{
 
+    constructor(){
+        super()
+
+        this.state={
+            name: 'Abigail Thelin'
+        }
+    }
+
     render(){
         return(
-            <div>
-                this is the profile
+            <div className="Profile">
+                <div className='nameContainer'>
+                    <div className="name">{this.state.name}</div>
+                    <img className="editIcon" src={editIcon} alt="edit"/>
+                </div>
+                <div className="imgContainer">
+                    <div className="imgPlaceholder"></div>
+                </div>
             </div>
         )
     }
