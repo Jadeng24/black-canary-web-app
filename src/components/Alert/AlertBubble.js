81e2c0cd57350af30a1da3370f797300f407e03a
Abigail Thelin <abbyjthelin@gmail.com>
2017-10-03 19:29:25 -0600

    alert page made and part styling

--- src/components/Alert/AlertBubble.js
+++ src/components/Alert/AlertBubble.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
+import Alert from './Alert'
 
 export default class AlertBubble extends Component{
 
-    constructor(){
-        super()
-        this.state={
-            alert
-        }
-    }
 
     render(){
         return(
             <div className="alertBubble">
-                <p>From: </p>
-            </div>
+                    {this.props.state.map(function(value, index){
+                        return(
+                    <div className="container" key={index}>
+                        <p className="from">{value.from}</p>
+                        <div className="messageContainer">
+                            <p className="message">{value.message}</p>
+                            <p>map goes here</p>
+                        </div>
+                    </div>)
+                    })}
+            </div>  
         )
     }
 }
